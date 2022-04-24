a8150acd64a459d035cda5bc5dd660cfc640d511
Victor Jiménez <vcjimenez1@espe.edu.ec>
2022-04-24 07:54:22 -0500

    UpdateBranch

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from 'next/app';
+
+import '../styles/style.scss'
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <Component {...pageProps} />
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = (): JSX.Element => {
+
+    return (
+        <>
+            Awesome!
+        </>
+    );
+};
+
+export default Home;
