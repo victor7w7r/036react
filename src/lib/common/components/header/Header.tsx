b747c6fe574609bb7e029238487c7c357fa97365
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2024-02-21 09:38:59 -0500

    FixHeader

--- src/lib/common/components/header/Header.tsx
+++ src/lib/common/components/header/Header.tsx
@@ -13,7 +13,7 @@ export const Header = block(() => {
     <div className='flex items-center justify-center'>
       <nav className={`navbar ${control}`}>
         <div className='mx-auto flex max-w-7xl items-center justify-between px-2 sm:px-6 lg:px-8'>
-          <div className='flex flex-1 shrink-0 items-stretch  justify-start '>
+          <div className='flex flex-1 shrink-0 items-stretch justify-start '>
             <img src='/favicon.svg' width={30} height={30} alt='' />
             <Link to='/' className='adaptable-text ml-4 font-medium'>
               React Template
