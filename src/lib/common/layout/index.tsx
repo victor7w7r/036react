66a17bfc306a0a94151694e6fb5a3fd007385533
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2024-02-21 10:05:09 -0500

    RemoveTrailingSpace

--- src/lib/common/layout/index.tsx
+++ src/lib/common/layout/index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import { Header } from '@/common/components'
 
 export const Layout = () => (
-  <main className='flex h-screen flex-col gap-8 '>
+  <main className='flex h-screen flex-col gap-8'>
     <Header />
     <Outlet />
   </main>
