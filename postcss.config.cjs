95e76cd384179bc5ed71fa96b47361d9f7edd5ad
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2024-04-04 12:11:54 -0500

    FinishUpdateBlank

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
+// eslint-disable-next-line toplevel/no-toplevel-side-effect, import/no-commonjs
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
+    autoprefixer: {},
+    tailwindcss: {}
   }
 }
